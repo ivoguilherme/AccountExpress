83dc823e5397b5284eeb10b5ee0b22de43af3012
Ivo Guilherme <ivo.guiilherme@gmail.com>
2019-03-31 14:13:38 -0300

    Front-end > VehicleRegistration

---
+++ AccountExpress/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountExpress.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public IActionResult CustomerRegistration()
+        {
+            return View();
+        }
+    }
+}
---
+++ AccountExpress/Controllers/VehiclesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountExpress.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        public IActionResult VehicleRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult VehicleManager()
+        {
+            return View();
+        }
+    }
+}

3f8c237aeb92cccfa4ef7fc4b170eee9d37f66a9
matheus chaves comin santana <theusccs111@outlook.com>
2019-05-03 21:30:07 -0300

    Ajuste em viewbag select

--- AccountExpress/Controllers/RentsController.cs
+++ AccountExpress/Controllers/RentsController.cs
@@ -84,27 +84,21 @@ namespace AccountExpress.Controllers
             var rent = _rentService.Get(id);
 
             IEnumerable<Customer> customers = _customerService.Get();
-            ViewBag.Customers = customers.Select(c => new SelectListItem()
-            {
-                Text = c.Name,
-                Value = c.Id.ToString(),
-                Selected = rent.CustomerId == c.Id
-            }).ToList();
+            ViewBag.Customers = new SelectList(customers, "Id", "Name", rent.CustomerId);
 
             IEnumerable<Vehicle> vehicles = _vehicleService.Get();
-            ViewBag.Vehicles = vehicles.Select(v => new SelectListItem()
-            {
-                Text = string.Concat(v.Brands, " - ", v.Model),
-                Value = v.Id.ToString(),
-                Selected = rent.VehicleId == v.Id
-            }).ToList();
+            ViewBag.Vehicles = new SelectList(vehicles, "Id", "Model", rent.VehicleId);
 
-            ViewBag.RentType = new SelectList(Enum.GetValues(typeof(RentType)).Cast<RentType>().Select(v => new SelectListItem
-            {
-                Text = v.ObterDescricao(),
-                Value = ((int)v).ToString(),
-                Selected = rent.TypeOfRent == v
-            }).ToList(), "Value", "Text");
+            //ViewBag.RentTypeEdit = new SelectList(Enum.GetValues(typeof(RentType)).Cast<RentType>().Select(v => new SelectListItem
+            //{
+            //    Text = v.ObterDescricao(),
+            //    Value = ((int)v).ToString(),
+            //    Selected = rent.TypeOfRent == v
+            //}).ToList(), "Value", "Text");
+
+            ViewBag.RentType = new SelectList(Enum.GetValues(typeof(RentType)).Cast<RentType>()
+                .Select(v => new { Id = v, Text = v.ObterDescricao() })
+                .ToList(), "Id", "Text", rent.TypeOfRent);
 
             return View();
 

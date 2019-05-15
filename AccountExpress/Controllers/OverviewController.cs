7cf154ad109cb4feab5d0825b0446c9b29617266
Ivo Guilherme <ivo.guiilherme@gmail.com>
2019-05-15 10:39:55 -0300

    Ajustes 1.0.1

--- AccountExpress/Controllers/OverviewController.cs
+++ AccountExpress/Controllers/OverviewController.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AccountExpress.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountExpress.Controllers
 {
     public class OverviewController : Controller
     {
+        private readonly IRentService _rentService;
+        private readonly IVehicleService _vehicleService;
+        private readonly ICustomerService _customerService;
+        public OverviewController(IRentService rentService, IVehicleService vehicleService, ICustomerService customerService)
+        {
+            _rentService = rentService;
+            _vehicleService = vehicleService;
+            _customerService = customerService;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.TotalVehicles = _vehicleService.Get().Count();
+            ViewBag.AvailableVehicles = _vehicleService.Get().Where(dv => dv.isRented == false).Count();
+            ViewBag.UnavailableVehicles = _vehicleService.Get().Where(dv => dv.isRented == true).Count();
+
+            ViewBag.TotalCustomers = _customerService.Get().Count();
+
+            ViewBag.TotalRents = _rentService.Get().Count();
+
             return View();
         }
     }
